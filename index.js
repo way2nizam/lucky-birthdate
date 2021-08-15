43eea438a17a47655df02c7cc2f13385abc2e3bb
nizamy710 <nizamy710@gamil.com>
2021-08-16 01:31:30 +0530

    fix: added index javascript and css file

---
+++ index.js
@@ -0,0 +1,8 @@
+const inputDate = document.querySelector("#input-date");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("#check-btn");
+const showOutput = document.querySelector("#show-output");
+
+function clickHandler() {}
+
+checkButton.addEventListener("click", clickHandler);
