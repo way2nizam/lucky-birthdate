470199d2a140f03332424424a39e7003860e0d89
Nizam <nizamy710@gmail.com>
2021-08-16 16:20:33 +0530

    feat: lucky number project added

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const inputDate = document.querySelector("#input-date");
-const luckyNumber = document.querySelector("#lucky-number");
-const checkButton = document.querySelector("#check-btn");
-const showOutput = document.querySelector("#show-output");
+const inputDob = document.querySelector('#input-date');
+const luckyNumber = document.querySelector('#lucky-number');
+const checkButton = document.querySelector('#check-btn');
+const showOutput = document.querySelector('#show-output');
 
-function clickHandler() {}
+function trimDob(p) {
+  const dobArray = p.replaceAll('-', '');
+  return dobArray;
+}
 
-checkButton.addEventListener("click", clickHandler);
+function dobCharSum(dobArray) {
+  let sum = 0;
+  for (let digit of dobArray) {
+    sum = sum + Number(digit);
+  }
+  return sum;
+}
+
+function showMessage(message) {
+  showOutput.innerHTML = message;
+}
+
+function clickHandler() {
+  const inputLuckyNumber = luckyNumber.value;
+  const dobInString = inputDob.value.toString();
+
+  const trimmedDob = trimDob(dobInString);
+
+  const dobSumValue = dobCharSum(trimmedDob);
+
+  if (dobSumValue % inputLuckyNumber === 0) {
+    showMessage(inputLuckyNumber + ' is a lucky number.');
+  } else {
+    showMessage(inputLuckyNumber + ' is not a lucky number.');
+  }
+}
+
+checkButton.addEventListener('click', clickHandler);
